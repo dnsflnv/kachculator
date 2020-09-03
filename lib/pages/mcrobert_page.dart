34d2845e6e4cd9e74c6d9dd77fb918610b356e6d
Denis Filonov <denis@filonov.pro>
2020-09-03 15:35:33 +0200

    McRobert page fixes.

--- lib/pages/mcrobert_page.dart
+++ lib/pages/mcrobert_page.dart
@@ -28,59 +28,68 @@ class _McRobertPageState extends State<McRobertPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mpAppBar(title: Text(S.of(context).mcrobertPageTitle)),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Text(S.of(context).mcrobertPageDesc),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
-                  controller: tcHeight,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: S.of(context).bmiHeight,
-                  ),
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (value) {
-                    if (value.isEmpty || double.parse(value) <= 0) {
-                      return S.of(context).bmiHeightValidation;
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              mpButton(
-                label: S.of(context).calculate,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    double height = double.parse(tcHeight.text);
-                    Map<String, List<double>> mac =
-                        mcRobert(context: context, heightCm: height);
-                    String res = '''
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 800.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Text(S.of(context).mcrobertPageDesc),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        controller: tcHeight,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: S.of(context).bmiHeight,
+                        ),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        validator: (value) {
+                          if (value.isEmpty || double.parse(value) <= 0) {
+                            return S.of(context).bmiHeightValidation;
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    mpButton(
+                      label: S.of(context).calculate,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          double height = double.parse(tcHeight.text);
+                          Map<String, List<double>> mac =
+                              mcRobert(context: context, heightCm: height);
+                          String res = '''
 |**${S.of(context).mcPart}**|**${S.of(context).mcrobertMin}**|**${S.of(context).mcrobertMax}**|
 |---|---|---|
 ''';
-                    mac.forEach((key, value) {
-                      res += '''
+                          mac.forEach((key, value) {
+                            res += '''
 |  $key  |  ${value[0].toStringAsFixed(1)}  |${value[1].toStringAsFixed(1)}  |
 ''';
-                    });
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultPage(
-                          result: res,
-                          title: S.of(context).mcrobertPageTitle,
-                        ),
-                      ),
-                    );
-                  }
-                },
+                          });
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ResultPage(
+                                result: res,
+                                title: S.of(context).mcrobertPageTitle,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ),
