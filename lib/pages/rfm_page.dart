67373a769fa172a27a489a69f1a6204a7df7e85a
Denis Filonov <denis@filonov.pro>
2020-09-03 15:26:59 +0200

    Rfm page fixes

--- lib/pages/rfm_page.dart
+++ lib/pages/rfm_page.dart
@@ -19,16 +19,17 @@ class _RfmPageState extends State<RfmPage> {
   TextEditingController tcHeight;
   TextEditingController tcWaistCircumference;
   TextEditingController tcAge;
-  double bmi = 0;
+  double bmi;
   Gender gender;
   String result = '';
 
   @override
   void initState() {
     super.initState();
+    bmi = 0;
     tcWeight = TextEditingController(text: '90');
     tcHeight = TextEditingController(text: '184');
-    tcWaistCircumference = TextEditingController(text: '100');
+    tcWaistCircumference = TextEditingController(text: '96');
     tcAge = TextEditingController(text: '41');
     gender = Gender.male;
   }
@@ -38,108 +39,123 @@ class _RfmPageState extends State<RfmPage> {
     return Scaffold(
       appBar: mpAppBar(title: Text(S.of(context).rfmPageTitle)),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Text(S.of(context).absiPageDesc),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    controller: tcHeight,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: S.of(context).bmiHeight,
-                    ),
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return S.of(context).bmiHeightValidation;
-                      }
-                      if (double.parse(value) <= 0) {
-                        return S.of(context).bmiHeightValidation;
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: TextFormField(
-                    controller: tcWaistCircumference,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: S.of(context).absiWaistCircumference,
-                    ),
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return S.of(context).absiWaistCircumferenceValidation;
-                      }
-                      if (double.parse(value) <= 0) {
-                        return S.of(context).absiWaistCircumferenceValidation;
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Row(
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 800.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
                   children: [
-                    Radio(
-                      value: Gender.female,
-                      groupValue: gender,
-                      onChanged: (value) {
-                        setState(() {
-                          gender = value;
-                        });
+                    Text(S.of(context).rfmPageDescription),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        controller: tcHeight,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: S.of(context).bmiHeight,
+                        ),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return S.of(context).bmiHeightValidation;
+                          }
+                          if (double.parse(value) <= 0) {
+                            return S.of(context).bmiHeightValidation;
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        controller: tcWaistCircumference,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: S.of(context).absiWaistCircumference,
+                        ),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return S
+                                .of(context)
+                                .absiWaistCircumferenceValidation;
+                          }
+                          if (double.parse(value) <= 0) {
+                            return S
+                                .of(context)
+                                .absiWaistCircumferenceValidation;
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: Gender.female,
+                          groupValue: gender,
+                          onChanged: (value) {
+                            setState(() {
+                              gender = value;
+                            });
+                          },
+                        ),
+                        Text(S.of(context).female),
+                        Radio(
+                          value: Gender.male,
+                          groupValue: gender,
+                          onChanged: (value) {
+                            setState(() {
+                              gender = value;
+                            });
+                          },
+                        ),
+                        Text(S.of(context).male),
+                      ],
+                    ),
+                    mpButton(
+                      label: S.of(context).calculate,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          double height = double.parse(tcHeight.text);
+                          int age = int.parse(tcAge.text);
+                          if (age > 85) age = 85;
+                          double waistCircumference =
+                              double.parse(tcWaistCircumference.text);
+                          Calc calc = Calc.rfm(
+                              context: this.context,
+                              heightAthleteCm: height,
+                              gender: gender,
+                              waistCircumferenceCm: waistCircumference);
+
+                          String res = """
+**${S.of(context).rfmPageTitle}**: ${calc.rfm.toStringAsFixed(1)}%
+
+**${S.of(context).bfpCategory}:** ${calc.bodyFat(calc.rfm)}
+"""; //
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ResultPage(
+                                result: res,
+                                title: S.of(context).rfmPageTitle,
+                              ),
+                            ),
+                          );
+                        }
                       },
                     ),
-                    Text(S.of(context).female),
-                    Radio(
-                      value: Gender.male,
-                      groupValue: gender,
-                      onChanged: (value) {
-                        setState(() {
-                          gender = value;
-                        });
-                      },
-                    ),
-                    Text(S.of(context).male),
                   ],
                 ),
-                mpButton(
-                  label: S.of(context).calculate,
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      double height = double.parse(tcHeight.text);
-                      int age = int.parse(tcAge.text);
-                      if (age > 85) age = 85;
-                      double waistCircumference =
-                          double.parse(tcWaistCircumference.text);
-                      Calc calc = Calc.rfm(
-                          context: this.context,
-                          heightAthleteCm: height,
-                          gender: gender,
-                          waistCircumferenceCm: waistCircumference);
-
-                      String res = """
-**${S.of(context).rfmPageTitle}**: ${calc.rfm.toStringAsFixed(1)}%
-"""; //
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ResultPage(
-                            result: res,
-                            title: S.of(context).rfmPageTitle,
-                          ),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ],
+              ),
             ),
           ),
         ),
