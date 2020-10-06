39ad431a24ad407fef60da73e268ff23b681f59e
Denis Filonov <denis@filonov.pro>
2020-10-06 11:31:30 +0200

    CooperPage

--- lib/pages/cooper_strong_page.dart
+++ lib/pages/cooper_strong_page.dart
@@ -14,19 +14,30 @@ class CooperStrongPage extends StatefulWidget {
 }
 
 class _CooperStrongPageState extends State<CooperStrongPage> {
-  final _formKey = GlobalKey<FormState>();
   TextEditingController tcMinutes;
+  bool errorMinutes;
+
+  bool _validation() {
+    if (tcMinutes.text.isEmpty || double.parse(tcMinutes.text) <= 0) {
+      setState(() {
+        errorMinutes = true;
+      });
+      return true;
+    }
+    return false;
+  }
 
   @override
   void initState() {
     super.initState();
     tcMinutes = TextEditingController(text: '4');
+    errorMinutes = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return mpScaffold(
+      appBar: mpAppBar(
         title: Text(S.of(context).cooperStrongPageTitle),
       ),
       body: SafeArea(
@@ -35,70 +46,59 @@ class _CooperStrongPageState extends State<CooperStrongPage> {
             constraints: BoxConstraints(maxWidth: 800.0),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Form(
-                key: _formKey,
-                child: ListView(
-                  children: [
-                    Text(S.of(context).cooperStrongPageDesc),
-                    SizedBox(
-                      height: 8.0,
+              child: ListView(
+                children: [
+                  Text(S.of(context).cooperStrongPageDesc),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    height: 360.0,
+                    child: Image.asset(
+                      'images/cooper.png',
+                      fit: BoxFit.none,
+                      //filterQuality: FilterQuality.low,
                     ),
-                    Container(
-                      height: 360.0,
-                      child: Image.asset(
-                        'images/cooper.png',
-                        fit: BoxFit.none,
-                        //filterQuality: FilterQuality.low,
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: mpTextField(
+                      controller: tcMinutes,
+                      labelText: S.of(context).cooperStrongMinutes,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8.0),
-                      child: TextFormField(
-                        controller: tcMinutes,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: S.of(context).cooperStrongMinutes,
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        validator: (value) {
-                          if (value.isEmpty || double.parse(value) <= 0) {
-                            return S.of(context).mcWristValidation;
-                          }
-                          return null;
-                        },
-                      ),
+                  ),
+                  if (errorMinutes)
+                    MpValidationMessage(
+                      message: S.of(context).cooperStrongMinutesValidation,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 16.0, left: 32.0, right: 32.0),
-                      child: mpButton(
-                        label: S.of(context).calculate,
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            String res = '';
-                            String force = cooperStrong(
-                                context: context,
-                                minutes: int.parse(tcMinutes.text));
-                            res = '''
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 16.0, left: 32.0, right: 32.0),
+                    child: mpButton(
+                      label: S.of(context).calculate,
+                      onPressed: () {
+                        if (_validation()) return null;
+                        String res = '';
+                        String force = cooperStrong(
+                            context: context,
+                            minutes: int.parse(tcMinutes.text));
+                        res = '''
 **${S.of(context).cooperMark}:** $force
 ''';
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ResultPage(
-                                  result: res,
-                                  title: S.of(context).cooperStrongPageTitle,
-                                ),
-                              ),
-                            );
-                          }
-                        },
-                      ),
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ResultPage(
+                              result: res,
+                              title: S.of(context).cooperStrongPageTitle,
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
